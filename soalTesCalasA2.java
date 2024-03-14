a2c8d377345539fe0c0245e19c94d94b93d2b621
Muhammad Fiqri <muhfikri6565@gmail.com>
2024-03-15 02:55:15 +0700

    comment added

--- soalTesCalasA2.java
+++ soalTesCalasA2.java
@@ -32,21 +32,33 @@ public class soalTesCalasA2 {
 
     //this method is to render the CLI Table UI
     public static void renderTable(String[][] Nilai) {
+        //define the width of the table by one unit of char
         int tableWidth = 61;
+        //define the height of the table by one unit of char
         int tableHeight = 7;
+        //define the location of every column
         int[] tableColumn = {1,9,18,27,36,61};
-        int[] textColLoc = {2,10,19,28,37,62};
+        //define the location of the text column index to be written on each row
+        int[] textColLoc = {3,10,19,28,37,62};
+        //define the row that can be written with text
         int[] tableRow = {2,4,5,6}; 
 
         for (int row = 1; row <= tableHeight; row++) {
             for (int col = 1; col <= tableWidth; col++) {
+
+                //print the writeablerow
                 if (soalTesCalasA2.contains(tableRow,row)) {
+
+                    //print the column border
                     if (soalTesCalasA2.contains(tableColumn,col)) {
                         System.out.print("|");
                     } else {
                         
+                        //print the text in the writeable row
                         for (int h = 0 ; h <= 3; h++) {
                             for (int i = 0; i <= 4; i++) {
+
+                                //print only if col index is in text location and in the writeable row and if it's outside the text range, print space
                                 if (col == textColLoc[i] && row == tableRow[h]) {
                                     System.out.print(Nilai[h][i]);
                                 } else if (col >= textColLoc[i] + soalTesCalasA2.getStringLengthInArray(Nilai[h], i) && col <= textColLoc[i+1]-1 && row == tableRow[h]) {
@@ -76,6 +88,7 @@ public class soalTesCalasA2 {
                     }
 
                 } else {
+                    //print the border row
                     if (col == 1 || col == tableWidth || col % 9 == 0 && col <= 36) {
                         System.out.print("+");
                     } else {
@@ -88,7 +101,10 @@ public class soalTesCalasA2 {
     }
 
     public static void main(String[] args) {
+        //define the raw data to be written in table, the header is the first array
         String[][] Nilai = { {"NAMA","UTS","UAS","TUGAS","NILAI AKHIR"}, {"Atta","60.0","70.0","90.0","70.5"}, {"Marse","80.0","70.0","90.0","77.5"}, {"Rakun", "70.0", "60.0", "90.0", "69.5"}  };
+        
+        //render the table
         soalTesCalasA2.renderTable(Nilai);
     }
 
